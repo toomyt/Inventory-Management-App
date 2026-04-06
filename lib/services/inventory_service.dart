7f09bda5cc48e0d2e768de607853bcd0608de31c
Thomas <tranthomas494@gmail.com>
2026-04-06 17:45:19 -0400

    Phase B: Implement Firestore service methods(add, stream, update, delete)

---
+++ lib/services/inventory_service.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:inventory_management_app/models/item.dart';
+
+class InventoryService {
+  final CollectionReference _collection = FirebaseFirestore.instance.collection('inventory');
+  Future<void> addItem(ItemModel item) async {
+  await _collection.add(item.toMap());
+  }
+  Stream<List<ItemModel>> streamItems() {
+  return _collection.snapshots().map(
+    (snap) => snap.docs.map((d) => ItemModel.fromFirestore(d)).toList(),
+    );
+  }
+
+  Future<void> updateItem(ItemModel item) async {
+    await _collection.doc(item.id).update(item.toMap());
+  }
+
+  Future<void> deleteItem(String id) async {
+    await _collection.doc(id).delete();
+  }
+}
